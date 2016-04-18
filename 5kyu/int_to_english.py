83e9bf8f00c83e72a9b19dbd3e3cac1ae53e7a64
ZhengC1 <zhengc1@appstate.edu>
2016-04-18 11:58:14 -0400

    intial commit

---
+++ 5kyu/int_to_english.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+
+single = ["", "one", "two", "three", "four","five", "six", "seven","eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+tens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+big = ["hundred", "thousand", "million", "billion"]
+
+def int_to_english(n):
+    if n < 20: 
+        return single[n]
+    elif n < 100: 
+        return tens[(n/10) - 2] + " " + int_to_english(n%10)
+    elif n < 1000: 
+        return tens[(n/10) - 2] + " " + int_to_english(n%10)
+
+print int_to_english(90)
