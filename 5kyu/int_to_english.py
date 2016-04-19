8d3378eb1326e05fc0c2b514f44bb3f7776f204a
ZhengC1 <zhengc1@appstate.edu>
2016-04-19 00:41:03 -0400

    added .rstrip() to get rid of whitespace()

--- 5kyu/int_to_english.py
+++ 5kyu/int_to_english.py
@@ -13,7 +13,7 @@ single = ["", "one", "two", "three",
           "seventeen", "eighteen", "nineteen"]
 
 tens = [
-    "", "twenty", "thirty", "fourty",
+    "", "twenty", "thirty", "forty",
     "fifty", "sixty", "seventy", "eighty",
     "ninety"]
 
@@ -26,22 +26,22 @@ def int_to_english(n):
         return (single[n])
     elif n < 100:
         return (tens[(n / 10) - 1] +
-                " " + int_to_english(n % 10))
+                " " + int_to_english(n % 10)).rstrip()
     elif n < 1000:
         return (int_to_english(n / 100) +
-                " " + big[1] + " " + int_to_english(n % 100))
+                " " + big[1] + " " + int_to_english(n % 100)).rstrip()
     elif n < 1000000:
         return (int_to_english(n / 1000) +
-                " " + big[2] + " " + int_to_english(n % 1000))
+                " " + big[2] + " " + int_to_english(n % 1000)).rstrip()
     elif n < 1000000000:
         return (int_to_english(n / 1000000) +
-                " " + big[3] + " " + int_to_english(n % 1000000))
+                " " + big[3] + " " + int_to_english(n % 1000000)).rstrip()
     elif n < 10000000000000:
         return (int_to_english(n / 1000000000) +
-                " " + big[4] + " " + int_to_english(n % 1000000000))
+                " " + big[4] + " " + int_to_english(n % 1000000000)).rstrip()
 
 print "{} {}".format("test 1", int_to_english(11))
-print "{} {}".format("test 2", int_to_english(122))
+print "{} {}".format("test 2", int_to_english(47))
 print "{} {}".format("test 3", int_to_english(333))
 print "{} {}".format("test 4", int_to_english(4444))
 print "{} {}".format("test 5", int_to_english(55555))
