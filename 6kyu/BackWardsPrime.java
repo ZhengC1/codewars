5dd11ed18911ce5079118e38dbb92f7f82c5c417
ZhengC1 <zhengc1@appstate.edu>
2016-04-20 13:33:45 -0400

    commit backwardsprime

---
+++ 6kyu/BackWardsPrime.java
@@ -0,0 +1,15 @@
+public class BackWarsPrime
+{
+  public static void main(String []args)
+  {
+
+  }
+  public static String backwardsPrime(long start, long end)
+  {
+    long[] arr = new long[];
+    for(i = start; start <= end; i++)
+    {
+        
+    }
+  }
+}
