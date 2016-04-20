725ff396edb542570f1c3a88506b5570e1920f60
ZhengC1 <zhengc1@appstate.edu>
2016-04-20 14:22:05 -0400

    i forgot how much i hate java

--- 6kyu/BackWardsPrime.java
+++ 6kyu/BackWardsPrime.java
@@ -1,15 +1,23 @@
-public class BackWarsPrime
-{
-  public static void main(String []args)
-  {
+import java.util.*;
 
-  }
-  public static String backwardsPrime(long start, long end)
-  {
-    long[] arr = new long[];
-    for(i = start; start <= end; i++)
+public class BackWardsPrime
+{
+    public static void main(String[] args)
     {
-        
+        System.out.println(backwardsPrime(2, 100));
     }
-  }
+
+    public static String backwardsPrime(long start, long end)
+    {
+        long size = start - end;
+        ArrayList<Long> arr = new ArrayList<Long>();
+        long i;
+        for(i = start; i <= end; i++)
+        {
+            arr.add(i);
+        }
+        Object[] objArr = arr.toArray();
+        String[] strArr = Arrays.copyOf(objArr, objArr.length, String[].class);
+        return Arrays.toString(strArr);
+    }
 }
